a9131ed83e7b62cc80e8eb4921adc0f432c9ed30
dominik <dominikpeter@users.noreply.github.com>
2018-02-27 17:16:49 +0100

    Update split_by_char_and_len.py

--- split_by_char_and_len.py
+++ split_by_char_and_len.py
@@ -2,7 +2,7 @@
 import os
 
 def split_by_char_and_length(s, m=20, pattern=" "):
-    def split_by_char_and_length_rec(s, m=20, pattern=" ", l=[]):
+    def split_rec(s, m=20, pattern=" ", l=[]):
         if len(s) < m:
             l.append(s)
             return l
@@ -11,12 +11,12 @@ def split_by_char_and_length(s, m=20, pattern=" "):
             idx = g[::-1].find(" ")
             if idx == -1:
                 l.append(s[:m])
-                return split_by_char_and_length_rec(s[m+len(pattern):], m=m, pattern=pattern, l=l)
+                return split_rec((s[m+len(pattern):], m=m, pattern=pattern, l=l)
             else:
                 l.append(s[:m-r-len(pattern)])
-                return split_by_char_and_length_rec(s[m-idx:], m=m, pattern=pattern, l=l)
+                return split_rec((s[m-idx:], m=m, pattern=pattern, l=l)
     l = []    
-    return split_by_char_and_length_rec(s=s, m=m, pattern=pattern, l=l)
+    return split_rec((s=s, m=m, pattern=pattern, l=l)
 
 class Splitter(object):
     def __init__(self, string, length, pattern):
