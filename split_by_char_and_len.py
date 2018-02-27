928f46d54d623b86a7ee7cd47cf617ebdea22161
dominik <dominikpeter@users.noreply.github.com>
2018-02-27 16:47:29 +0100

    Update split_by_char_and_len.py

--- split_by_char_and_len.py
+++ split_by_char_and_len.py
@@ -1,32 +1,28 @@
+
 import os
 import re
 
-s = 'das ist ein test zum splitten von text'
+s = 'Plattentrennmaschine VIP 260 230 V 0.65 kW Schnitttiefe 40 mm mit Diamanttrennscheibe 200 mm und Untergestell'
 
-def isplit(s, m=10, pattern=" ", i=0):
-    if i + s.find(pattern) > m:
-        return i - len(pattern)
-    else:
-        c = s.find(pattern) + len(pattern)
-        i += c
-        return isplit(s[c:], m=m, pattern=pattern, i=i)
-        
-        
-def tsplit(s, l=[], m=10, pattern=" "):
-    if len(s) > m:
-        idx = isplit(s, m=m, pattern=pattern)
-        t = s[:idx]
-        l.append(t)
-        return tsplit(s[idx+1:], l, m=m)
-    else:
-        l.append(s)
-        return l
-        
-        
-def split_by_char_and_length(s, m=10, pattern=" "):
-    l = []
-    l = tsplit(s, l=l, m=m, pattern=pattern)
-    return l
+def split_by_char_and_length(s, m=20, pattern=" "):
     
-" / ".join(split_by_char_and_length(s, m=10, pattern=" "))
+    def split_by_char_and_length_rec(s, m=20, pattern=" ", l=[]):
+        if len(s) < m:
+            l.append(s)
+            return l
+        else:
+            g = s[:m]
+            r = g[::-1].find(" ")
+            if r == -1:
+                l.append(s[:m])
+                return split_by_char_and_length_rec(s[m+len(pattern):], m=m, pattern=pattern, l=l)
+            else:
+                l.append(s[:m-r-len(pattern)])
+                return split_by_char_and_length_rec(s[m-r:], m=m, pattern=pattern, l=l)
+    l = []
+    
+    return split_by_char_and_length_rec(s=s, m=m, pattern=pattern, l=l)
 
+" || ".join(split_by_char_and_length(s, m=40))
+
+[len(i) for i in split_by_char_and_length(s, m=30)]
